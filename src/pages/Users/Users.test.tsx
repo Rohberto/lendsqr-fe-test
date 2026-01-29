3c35ab604608f31c84bba5e272beee96e86bde85
Rohberto <oluwaseunrobert44@gmail.com>
2026-01-29 09:35:34 +0100

    fix: removed unused userEvent variable that wasnt used, it caused deployment error

--- src/pages/Users/Users.test.tsx
+++ src/pages/Users/Users.test.tsx
@@ -1,6 +1,5 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import Users from './Users';
 import { UsersAPI } from '../../services/users.api';
